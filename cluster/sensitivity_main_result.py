21662b8a1dec69ebf749a81edecca877bef8b055
Nipun Batra <nipunb@iiitd.ac.in>
2016-07-08 16:42:08 +0530

    ddds

--- cluster/sensitivity_main_result.py
+++ cluster/sensitivity_main_result.py
@@ -1,25 +1,55 @@
-appliances = ["hvac","light","fridge","dr","wm","dw"]
-features = ["Static", "Monthly+Static", "Monthly", "AMI",
-            "AMI+Static", "All", "AMI+Monthly"]
+import sys
+
+CLUSTER = True
+if CLUSTER:
+    sys.path.insert(0, '/if6/nb2cz/anaconda/lib/python2.7/site-packages')
+
+import numpy as np
+import pandas as pd
+import pickle
+from collections import OrderedDict
+
+out_overall = pickle.load(open('../data/input/all_regions.pkl', 'r'))
+
+region = "Austin"
+
+df = out_overall[region]
+df = df.rename(columns={'house_num_rooms': 'num_rooms',
+                                  'num_occupants': 'total_occupants',
+                                  'difference_ratio_min_max': 'ratio_difference_min_max'})
+
+df = df[(df.full_agg_available == 1) & (df.md_available == 1)]
+
+appliances = ["hvac","fridge","wm","dw","mw","oven"]
+features = ["Static", "Monthly+Static", "Monthly"]
 SLURM_OUT = "../slurm_out"
 from subprocess import Popen
 
 
+
 for feature in features:
     for appliance in appliances:
-        OFILE = "%s/%s_%s.out" % (SLURM_OUT, appliance, feature)
-        EFILE = "%s/%s_%s.err" % (SLURM_OUT, appliance,  feature)
-        SLURM_SCRIPT = "%s_%s.pbs" % (appliance,feature)
-        CMD = 'python ../code/main_result.py %s %s' % (appliance,feature)
-        lines = []
-        lines.append("#!/bin/sh\n")
-        lines.append('#SBATCH --time=0-06:0:00\n')
-        lines.append('#SBATCH --mem=16\n')
-        lines.append('#SBATCH -o '+'"' +OFILE+'"\n')
-        lines.append('#SBATCH -e '+'"' +EFILE+'"\n')
-        lines.append(CMD+'\n')
 
-        with open(SLURM_SCRIPT, 'w') as f:
-           f.writelines(lines)
-        command = ['sbatch', SLURM_SCRIPT]
-        print Popen(command)
+        if appliance=="hvac":
+            start, stop=5, 11
+        else:
+            start, stop=1, 13
+
+        appliance_df= df.ix[df[['%s_%d' %(appliance,month) for month in range(start,stop)]].dropna().index]
+        for home in appliance_df.index:
+            OFILE = "%s/%s_%s_%d.out" % (SLURM_OUT, appliance, feature, home)
+            EFILE = "%s/%s_%s_%d.err" % (SLURM_OUT, appliance,  feature, home)
+            SLURM_SCRIPT = "%s_%s_%d.pbs" % (appliance,feature, home)
+            CMD = 'python ../code/journal_gemello_all_homes_leave_one_out.py %d %s %s' % (home, appliance,feature)
+            lines = []
+            lines.append("#!/bin/sh\n")
+            lines.append('#SBATCH --time=0-06:0:00\n')
+            lines.append('#SBATCH --mem=16\n')
+            lines.append('#SBATCH -o '+'"' +OFILE+'"\n')
+            lines.append('#SBATCH -e '+'"' +EFILE+'"\n')
+            lines.append(CMD+'\n')
+
+            with open(SLURM_SCRIPT, 'w') as f:
+               f.writelines(lines)
+            command = ['sbatch', SLURM_SCRIPT]
+            print Popen(command)
